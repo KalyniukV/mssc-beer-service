8f80f6361e50d8a6f08d2154020de68a2eabef35
Vitaliy Kalyniuk <kvit89@gmail.com>
2021-01-02 20:44:37 +0200

    Create Brew Beer Listener

--- src/main/java/com/example/msscbeerservice/events/NewInventoryEvent.java
+++ src/main/java/com/example/msscbeerservice/events/NewInventoryEvent.java
@@ -3,6 +3,7 @@ package com.example.msscbeerservice.events;
 import com.example.msscbeerservice.web.model.BeerDto;
 
 public class NewInventoryEvent extends BeerEvent {
+
     public NewInventoryEvent(BeerDto beerDto) {
         super(beerDto);
     }
