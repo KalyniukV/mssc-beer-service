35c3737b51612b19cb19b20452c4f45200e13160
Vitaliy Kalyniuk <kvit89@gmail.com>
2021-01-02 21:02:53 +0200

    Fixing Jackson configuration

--- src/main/java/com/example/msscbeerservice/events/BeerEvent.java
+++ src/main/java/com/example/msscbeerservice/events/BeerEvent.java
@@ -1,18 +1,17 @@
 package com.example.msscbeerservice.events;
 
 import com.example.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class BeerEvent implements Serializable {
 
     static final long serialVersionUID = 6778862444420929175L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 }
