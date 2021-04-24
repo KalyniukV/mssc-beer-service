0dae6afd494c8731e7ba5672093427429312114f
Vitaliy Kalyniuk <kvit89@gmail.com>
2021-04-24 21:38:30 +0300

    Configuring OpenFeign Client

---
+++ src/main/java/com/example/msscbeerservice/services/inventoiry/BeerInventoryServiceFeign.java
@@ -0,0 +1,35 @@
+package com.example.msscbeerservice.services.inventoiry;
+
+import com.example.msscbeerservice.services.inventoiry.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Profile("local-discovery")
+@Service
+public class BeerInventoryServiceFeign implements BeerInventoryService {
+
+    private final InventoryServiceFeignClient inventoryServiceFeignClient;
+
+    @Override
+    public Integer getOnHandInventory(UUID beerId) {
+        log.debug("Calling Inventory Service - BeerId: " + beerId);
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity = inventoryServiceFeignClient.getOnHandInventory(beerId);
+
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+
+        return onHand;
+    }
+}
